b95617e637ae396ae4c916c84eda5dd926819a69
1108-Fukuda-Kaito <kaito.6220f@gmail.com>
2024-12-06 03:10:29 +0000

    Updated madlibs.c

--- madlibs.c
+++ madlibs.c
@@ -5,7 +5,10 @@
 #define INPUT_FILE "madlib1.txt"
 #define ROW_LENGTH 100
 #define COL_LENGTH 100
+#define ARRAY_SIZE 50
 void scanFile(FILE *fptr, int rows, int columns, char array[][columns]);
+void findLetters(int rowLength, int columnLength, char madlibsArr[][columnLength], int size, char letters[], int rowNums[]);
+int rowSize(char array[]);
 
 int main(){
 	FILE *inFp;
@@ -16,8 +19,12 @@ int main(){
 		return 0;
 	}
 	scanFile(inFp, ROW_LENGTH, COL_LENGTH, madlibs);
-	for(int i=0; i<100; i++){
+	findLetters(ROW_LENGTH, COL_LENGTH, madlibs, ARRAY_SIZE, letterArr, rowNumArr);
+	for(int i=0; i<50; i++){
 		printf("%s", madlibs[i]);
+	}
+	for(int i=0; i<10; i++){
+		printf("%c%d ", letterArr[i], rowNumArr[i]);
 	}
 	fclose(inFp);
 	return 0;
@@ -39,3 +46,23 @@ void scanFile(FILE *fptr, int rows, int columns, char array[][columns]){
 	
 }
 
+void findLetters(int rowLength, int columnLength, char madlibsArr[][columnLength], int size, char letters[], int rowNums[]){
+	int rowCounter = 0;
+	int colCounter = 0;
+	while(madlibsArr[rowCounter][0] != '\0'){
+		if(rowSize(madlibsArr[rowCounter]) >= 2 && (madlibsArr[rowCounter][0] == 'A' || madlibsArr[rowCounter][0] == 'N' || madlibsArr[rowCounter][0] == 'V')){
+		 letters[colCounter] = madlibsArr[rowCounter][0];
+		 rowNums[colCounter] = rowCounter;
+		 colCounter++;
+		}
+		rowCounter++;
+	}
+}
+
+int rowSize(char array[]){
+	int counter = 0;
+	for(int i=0; array[i] != '\0'; i++){
+		counter++;
+	}
+	return counter;
+}
